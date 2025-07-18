652ad863ff361e81fcfb542a275907f856ebd30b
Thanathorn1 <thanatornfever@gmail.com>
2025-07-18 11:39:55 +0700

    Edit age

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,40 +1,34 @@
 <template>
   <div class="q-pa-md" style="max-width: 400px">
-
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
       <q-input
         filled
         v-model="name"
         label="Your name *"
         hint="Name and surname"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
       />
 
       <q-input
         filled
         type="number"
-        v-model="age"
-        label="Your age *"
+        v-model="อายุ"
+        label="อายุของคุณ *"
         lazy-rules
         :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
+          (val) => (val !== null && val !== '') || 'กรุณาใส่อายุของคุณ',
+          (val) => (val > 0 && val < 100) || 'กรุณาใส่อายุจริง',
         ]"
       />
 
       <q-toggle v-model="accept" label="I accept the license and terms" />
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Submit" type="submit" color="primary" />
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
     </q-form>
-
   </div>
 </template>
 
@@ -43,7 +37,7 @@ import { useQuasar } from 'quasar'
 import { ref } from 'vue'
 
 export default {
-  setup () {
+  setup() {
     const $q = useQuasar()
 
     const name = ref(null)
@@ -55,31 +49,30 @@ export default {
       age,
       accept,
 
-      onSubmit () {
+      onSubmit() {
         if (accept.value !== true) {
           $q.notify({
             color: 'red-5',
             textColor: 'white',
             icon: 'warning',
-            message: 'You need to accept the license and terms first'
+            message: 'You need to accept the license and terms first',
           })
-        }
-        else {
+        } else {
           $q.notify({
             color: 'green-4',
             textColor: 'white',
             icon: 'cloud_done',
-            message: 'Submitted'
+            message: 'Submitted',
           })
         }
       },
 
-      onReset () {
+      onReset() {
         name.value = null
         age.value = null
         accept.value = false
-      }
+      },
     }
-  }
+  },
 }
 </script>
